f6ca6b38c57bc471e97297cb8c049bc69a5c5353
hoandt <hdt1302@gmail.com>
2025-05-09 12:31:51 +0700

    fix server port

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = process.env.HOSTNAME || "localhost";
-const port = parseInt(process.env.PORT, 10) || 3000;
-const corsOrigin = process.env.CORS_ORIGIN || (dev ? "http://localhost:3000" : "*");
+const hostname = process.env.HOSTNAME || "0.0.0.0";
+const port = parseInt(process.env.PORT, 10) || 3005;
+const corsOrigin = process.env.CORS_ORIGIN || (dev ? "http://localhost:3005" : "*");
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
